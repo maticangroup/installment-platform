750cd48d94afb1f60cf7f20c67f9a6219ff6ad3b
Matican Matican <maticangroup@gmail.com>
2019-10-20 19:43:07 +0330

    hossein, changing the app settings

--- src/Kernel.php
+++ src/Kernel.php
@@ -27,7 +27,7 @@ class Kernel extends BaseKernel
         Settings::set('CLIENT_IP', '');
         Settings::set('CLIENT_ACCESS_TOKEN', '');
         Settings::set('APPLICATION_DOMAIN', 'http://127.0.0.1:8000');
-        Settings::set('SERVER_DOMAIN', 'http://core.maticangroup.com');
+        Settings::set('SERVER_DOMAIN', 'http://core.aradmag.com');
         AuthUser::check_if_user_is_logged_in();
         /*### MATICAN ###*/
 
